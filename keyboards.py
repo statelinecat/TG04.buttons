59da5b7a6d84858b9320154712cee823fcb0eee7
statelinecat <statelinecat@gmail.com>
2024-10-31 03:39:04 +0300

    add code2

---
+++ keyboards.py
@@ -0,0 +1,7 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+
+
+main = ReplyKeyboardMarkup(keyboard=[
+    [
+]
